93278ed8e7d391374e05dc62740895bd7e01d773
Vanessa Fotso <vfotso@mitre.org>
2024-07-15 19:53:07 -0400

    Fixed typo
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>

--- loadserver.rb
+++ loadserver.rb
@@ -133,12 +133,12 @@ end
 # json_directory = "2023-07-CMS-July-Connectathon/PFE/scene-5"
 # json_directory = "2023-07-CMS-July-Connectathon/GracitySDOH"
 # json_directory = "2023-09 HL7 Sept Connectathon"
-json_directory = "2024-05 HL7 May Connectathon/SMP-Samples"
+json_directory = "2024-07 CMS July Connectathon"
 
 # Provide the base URL of the server
-# base_url = "https://gw.interop.community/MiHIN/open/"
+base_url = "https://gw.interop.community/MiHIN/open"
 # base_url = "https://gravity-ehr-server.herokuapp.com/fhir"
-base_url = 'http://hapi.fhir.org/baseR4/'
+# base_url = 'http://hapi.fhir.org/baseR4'
 
 # Call the method to load JSON files onto the server and get the count of unsuccessful uploads
 total, unsuccessful_count = load_json_files(json_directory, base_url)
